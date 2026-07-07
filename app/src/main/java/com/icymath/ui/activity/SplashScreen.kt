ceb9559ad27f4330ed3858b75951bd761c1bae19
alcovaibe <132335071+alcovaibe@users.noreply.github.com>
2026-07-07 18:33:46 +0300

    Add Preview for SplashScreenContent

--- app/src/main/java/com/icymath/ui/activity/SplashScreen.kt
+++ app/src/main/java/com/icymath/ui/activity/SplashScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.icymath.R
 import com.icymath.ui.theme.IcyMathTheme
@@ -29,3 +30,13 @@ fun SplashScreenContent() {
         )
     }
 }
+
+@Preview(showBackground = true, name = "Splash Light")
+@Preview(showBackground = true, uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES, name = "Splash Dark")
+@Composable
+fun SplashScreenPreview() {
+    IcyMathTheme {
+        SplashScreenContent()
+    }
+}
+
